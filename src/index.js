eb9dd9d58f1f4b640ce44516b50cc033302afc39
manitu85 <manitu.psy@gmail.com>
2020-01-30 23:21:18 +0100

    #

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 // Components
-import App from './components/App';
-import FavoriteRecipeList from './components/FavoriteRecipeList';
-import Error404 from './components/Error404';
+import App from './components/App'
+import FavoriteRecipeList from './components/FavoriteRecipeList'
+import Error404 from './components/Error404'
 // Styles
 import './styles/app.scss'
 // ##############################################################
 
 // Redux store
-const store = createStore(rootReducer);
-store.subscribe(() => console.log('store:', store.getState()));
+const store = createStore(rootReducer)
+
+// Check store state
+store.subscribe(() => console.log('store:', store.getState()))
 
 // ReactDOM.render
 render(
@@ -28,5 +30,5 @@ render(
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
-);
+)
 
